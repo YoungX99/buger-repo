d5130285d0ead4138b1b24e91fc2590c231aaedd
youngx99 <1286739715@qq.com>
2022-09-25 15:35:55 +0800

    feat: add a base case

--- Vue/reactive/05-nest-effect/main.js
+++ Vue/reactive/05-nest-effect/main.js
@@ -71,3 +71,20 @@ function cleanUp(effectFn) {
   // 可以清空依赖关系了
   effectFn.deps.length = 0
 }
+
+// CASE
+// effectFn1 嵌套了 effectFn2
+effect(function effectFn1() {
+  console.log('effectFn1 执行')
+  effect(function effectFn2() {
+    console.log('effectFn2 执行')
+    // 在 effectFn2 中读取 obj.bar 属性
+    temp2 = obj.bar
+  })
+  // 在 effectFn1 中读取 obj.foo 属性
+  temp1 = obj.foo
+})
+
+setTimeout(() => {
+  obj.foo = 'hhh'
+}, 1000)
