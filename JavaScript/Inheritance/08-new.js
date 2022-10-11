440ef09d6eedc1a19c9f2868cfedd5f6d3f6d8de
youngx99 <1286739715@qq.com>
2022-10-11 16:02:23 +0800

    perf: less code for new operator

--- JavaScript/Inheritance/08-new.js
+++ JavaScript/Inheritance/08-new.js
@@ -1,13 +1,16 @@
-function objectFactory(...args) {
-  const CONSTRUCTOR = args.shift()
-  var obj = Object.create(CONSTRUCTOR.prototype)
-  var ret = CONSTRUCTOR.apply(obj, args)
-  return typeof ret === 'object' ? ret || obj : obj
+function objectFactory(CONSTRUCTOR, ...args) {
+  const obj = Object.create(CONSTRUCTOR.prototype) // 创建一个空实例对象，并将 __proto__ 指向构造函数的原型对象
+  const ret = CONSTRUCTOR.apply(obj, args) // 执行构造函数，并将 this 指向实例对象
+  return ret instanceof Object ? ret : obj // 判断返回结构
 }
 
 function Person(name, age) {
   this.name = name
   this.age = age
 }
+Person.prototype.getName = function () {
+  console.log(this.name)
+}
 let p = objectFactory(Person, '布兰', 12) // new Person('布兰', 12)
 console.log(p) // { name: '布兰', age: 12 }
+p.getName()
